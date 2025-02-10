a875383876983c1e1a2c6ade1aad8280c93a0cc4
Mostafizur Rahman <94890704+Mostafizur979@users.noreply.github.com>
2025-02-10 17:06:17 +0600

    3rd week

--- user/urls.py
+++ user/urls.py
@@ -2,11 +2,12 @@
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls import handler404
-from user.views import user_home
+from user.views import user_home,checkout,order_request
 
 urlpatterns = [
         
     path('',user_home,name="user_home"),
-    
+    path('checkout/',checkout,name="checkout"),
+    path('order-request/',order_request,name="order_request"),
 ]
 
